7cf2844d361bd8cb36d0c7d86ade5d70384bce19
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-04-29 20:49:49 +0200

    Create state_manager.py

---
+++ evo_core/memory/state_manager.py
@@ -0,0 +1,9 @@
+def save_journal_entry(self, entry):
+    # store in DB
+    pass
+
+def save_financial_split(self, split):
+    pass
+
+def get_upgrade_funds(self):
+    return {'lower': 0, 'upper': 0}
